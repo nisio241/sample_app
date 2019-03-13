3685033024abc55f723dfcbe052bbc7051bc3acc
Yuki <240noadress@gmail.com>
2019-03-13 09:12:03 +0900

    Finish user edit,update,index,and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,9 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
   def current_user
     if(user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -26,11 +29,6 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  # def log_out
-  #   session.delete(:user_id)
-  #   @current_user = nil
-  # end
-
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -41,5 +39,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwading_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
