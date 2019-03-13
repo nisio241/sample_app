978b4ecb84859fb472df8cded7f37b00fd493f15
Yuki <240noadress@gmail.com>
2019-03-13 20:13:37 +0900

    mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,8 +20,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivary_method = :test
-  host = 'localhost:3000'
+  config.action_mailer.delivery_method = :test
+  host = '192.168.33.10:3000'
   config.action_mailer.default_url_options = { host: host, protcol: 'http'}
 
 
